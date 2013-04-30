2cbb6aae77cf4dfbc646eeae4da9932357d7ec24
Daniel Casimir Grigg <mail@danielgrigg.com>
2013-04-30 22:08:42 +1000

    Improve performance of film

--- src/sliimp/film.clj
+++ src/sliimp/film.clj
@@ -8,7 +8,7 @@
 
 (defrecord Pixel [^float x ^float y ^float z ^float w])
 
-(defn pixel 
+(defn ^Pixel pixel 
    ([^double x ^double y ^double z]
       (Pixel. x y z 1.0))
    ([^double x ^double y ^double z ^double weight]
@@ -20,19 +20,13 @@
 
 (defrecord Film [^Rect bounds #^"[Lsliimp.film.Pixel;" pixels])
 
-(defn funky [w h]
-  (let [#^"[Lsliimp.film.Pixel;" ps (make-array Pixel (* w h))] 
-    (amap ps idx ret (pixel 0.0 0.0 0.0 0.0))))
 
-(defn film [& {:keys [bounds clear-colour]}]
+(defn ^Film film [& {:keys [bounds clear-colour]}]
    "Create a film, using a blank pixel array if pixels is missing."
    (let [#^"[Lsliimp.film.Pixel;" ps (make-array Pixel (* (height bounds) (width bounds)))
          ^Pixel p clear-colour] 
      (Film. bounds (amap ps idx ret p))))
 
-
- 
-;; (defn pixel-black [] (Pixel. [0.0 0.0 0.0] 1.0))
 ;; 
 ;; (defn add-pixel [^Pixel p ^Pixel q]
 ;;   (Pixel. (v3add (:xyz p) (:xyz q)) (+ (:weight p) (:weight q))))
@@ -40,14 +34,14 @@
 ;; (defn pixel-colour [^Pixel p]
 ;;   (v3muls (:xyz p) (:weight p)))
 ;; 
-;; (defn normalize [^Pixel p]
-;;   (if (< (:weight p) 0.0001)
-;;     (pixel-black)
-;;     (Pixel. (v3muls (:xyz p) (/ (:weight p))) 1.0)))
-;; 
-
-;; 
-
+ (defn ^Pixel normalize [^Pixel p]
+   (if (< (:w p) 0.0001)
+     (pixel 0.0 0.0 0.0 0.0)
+     (let [inv-w (float (/ (:w p)))]
+       (Pixel. (* (:x p) inv-w) 
+               (* (:y p) inv-w)
+               (* (:z p) inv-w)
+               1.0))))
 ;; 
 ;; (defn pixel-idx [^Film f ^long x ^long y]
 ;;   (+ (* y (int (width (:bounds f)))) x))
@@ -69,20 +63,34 @@
                        pixels))
 
  (defn spit-film [^Film f path]
-   (write-exr {:path path
-               :width (int (width (:bounds f)))
-               :height (int (height (:bounds f)))
-               :pixels (float-array
-                        (reduce (fn [acc x] 
-                                  (conj acc (:x x) (:y x) (:z x) 1.0)) [] (:pixels f)))}))
+   (let [w (int (width (:bounds f)))
+         h (int (height (:bounds f)))
+         l (int (* w h))
+         #^"[Lsliimp.film.Pixel;" ps (:pixels f)
+         ^floats fs (float-array (* l 4))]
+     (doseq [n (range l)]
+       (let [n' (int n)
+             ^Pixel p (normalize (aget ps n'))
+             y (int (* n' 4))]
+               (aset fs (int (+ y 0)) (float (:x p)))
+               (aset fs (int (+ y 1)) (float (:y p)))
+               (aset fs (int (+ y 2)) (float (:z p)))
+               (aset fs (int (+ y 3)) (float 1.0))))
+     
+     (write-exr {:path path
+                 :width w
+                 :height h
+                 :pixels fs})))
 
 (defn test-spit [w h pxl ]
   (spit-film (film :bounds (rect {:width w :height h}) 
                    :clear-colour pxl)
              "/tmp/funk.exr"))
 
- (defn -main [& args]
-   (test-spit 4096 4096 (pixel (rand) (rand) (rand) 1.0)))
+(defn pixel-rand [] (pixel (rand) (rand) (rand) 1.0))
+
+(defn -main [& args]
+  (test-spit 1024 1024 (pixel-rand)))
                   
 
 ;; 
