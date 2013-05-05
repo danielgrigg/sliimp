00ad442b90bb5914ac871a5650863d73fe39cf68
Daniel Casimir Grigg <mail@danielgrigg.com>
2013-05-05 17:46:45 +1000

    Bump for release

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject sliimp "0.1.2"
+(defproject sliimp "0.1.3"
   :description "An image processing library"
   :url "http://sliplanesoftware.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [slimath "0.2.0"]
-                 [slicna "0.1.2"]]
-  :jvm-opts ["-Xmx2g"]
+  :dependencies [[org.clojure/clojure "LATEST"]
+                 [slimath "LATEST"]
+                 [slicna "LATEST"]]
   :main sliimp.film)
 
 
