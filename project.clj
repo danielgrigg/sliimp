ac219e41822510f76b10d373eb35584c42db567f
Daniel Grigg <mail@danielgrigg.com>
2013-08-11 14:29:22 +1000

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sliimp "0.2.5"
+(defproject sliimp "0.2.6"
   :description "An image processing library"
   :url "http://sliplanesoftware.com/"
   :license {:name "Eclipse Public License"
